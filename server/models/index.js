e3fcd150f2363cdfa215ea5dbb123cfd1596abda
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-11-21 14:31:03 +0100

    modify index.js in models folder

--- server/models/index.js
+++ server/models/index.js
@@ -4,16 +4,24 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const dbName = (process.env.NODE_ENV === 'test'
+  ? process.env.DB_NAME_TEST
+  : process.env.DB_NAME);
+
+const sequelize = new Sequelize(dbName, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorsAliases: false // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+});
+
 
 fs
   .readdirSync(__dirname)
@@ -21,15 +29,16 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    var model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+Object.keys(db).forEach(model => {
+  if(db[model].associate) {
+    db[model].associate(db);
   }
 });
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
