59afcd47ff708e6db51bab4e5b333c5b3c4e9059
Ritam Verma <ritamvvv@gmail.com>
2020-11-23 12:29:44 +0100

    login test complete

--- client/src/containers/Login.test.js
+++ client/src/containers/Login.test.js
@@ -1,53 +1,14 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { screen , waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
-import { Provider } from "react-redux";
-import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import reducer from '../redux/reducers';
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import Login from './Login';
 import App from '../App';
-import { registerUser, BASE_URL } from '../redux/actions'
-const middleware = [thunk];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+import {  BASE_URL } from '../redux/actions'
+import customRender from '../utils/customRender'
 
 jest.mock('axios');
-
-const customStore = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(...middleware))
-);
-
-const customRender = (
-  ui, 
-  {
-    route = '/',
-    history = createMemoryHistory({initialEntries: [route]}),
-    initialState,
-    store = customStore,
-    ...renderOptions
-
-  } = {}) => {
-    const Wrapper = ({children}) => (
-      <Provider store = {store}>
-        <Router history = {history}>{children}</Router>
-      </Provider>
-    )
-
-    return {
-      ...render(ui, {
-        wrapper: Wrapper,
-        ...renderOptions, 
-      }),
-      history,
-      store,
-    };
-  }
 
   describe ('login component happy path', () => {
     test('should render pick gender after login correctly', async () => {
@@ -59,21 +20,19 @@ const customRender = (
       const history = createMemoryHistory();
       history.push('/login');
 
-      customRender(<Login history={history} />);
-
-    // customRender(<App />, { history });
+    customRender(<App />, { history });
     
-    const email = screen.getByLabelText(/Email/i);
+    const email = screen.getByLabelText(/email/i);
     expect(email).toBeInTheDocument();
     userEvent.type(email, 'email@email.com')
     expect(email).toHaveValue('email@email.com');
 
-    const password = screen.getByLabelText(/Password/i);
+    const password = screen.getByLabelText(/password/i);
     expect(password).toBeInTheDocument();
     userEvent.type(password, 'password')
     expect(password).toHaveValue('password');
 
-    const loginLink = screen.getByTestId(/Login/i);
+    const loginLink = screen.getByTestId(/login/i);
     expect(loginLink).toBeInTheDocument();
     userEvent.click(loginLink);
 
@@ -82,8 +41,9 @@ const customRender = (
       "password":"password"
     })
     expect(mockAPIpost).toHaveBeenCalledTimes(1);
-
-
+    await waitFor(()=> expect(history.location.pathname).toMatch('/game'));
+    const game = screen.getByText(/What type of name are you looking for/i);
+    expect(game).toBeInTheDocument();
     })
 
   })
--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducer from './redux/reducers';
-import { BrowserRouter } from 'react-router-dom';
+
 
 const middleware = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
