463c2233a572b890fd005ea14483f058bf1ed3b7
Ritam Verma <ritamvvv@gmail.com>
2020-11-24 10:34:23 +0100

    snapshots update

--- client/src/__tests__/game.test.js
+++ client/src/__tests__/game.test.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup} from '@testing-library/react'
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import Game from '../containers/Game';
 
-import Game from '../containers/Game'
 
-describe('Game', () => {
-  afterEach(() => {
-    cleanup();
-  });
+import customRender from '../utils/customRender'
 
-  test('render correctly', () => {
-    const game = render(<Game />);
-    expect(game.baseElement).toMatchSnapshot();
-  });
+afterEach(cleanup)
 
-});
+it('register snapshot', () => {
+  const history = createMemoryHistory();
+  history.push('/profile');
+  const { asFragment } = customRender(<App />, { history });
+  
+  expect(asFragment(<Game />)).toMatchSnapshot()
+})
--- client/src/__tests__/home.test.js
+++ client/src/__tests__/home.test.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
-
-import Home from '../containers/Home'
-
-describe('Home', () => {
-  afterEach(() => {
-    cleanup();
-  });
-
-  test('render correctly', () => {
-    const home = render(<Home />);
-    expect(home.baseElement).toMatchSnapshot();
-  });
-
-  test('should render title', () => {
-    render(<Home />);
-    expect(screen.findByText("Find your favourite baby names")).toBeTruthy();
-  });
-
-  test('renders start button', () => {
-    render(<Home/>)
-    expect(screen.getByText('Start')).toBeInTheDocument();
-  });
-
-  test('renders sign up button', () => {
-    render(<Home/>)
-    expect(screen.getByText('Sign up')).toBeInTheDocument();
-  });
+import { cleanup} from '@testing-library/react'
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import Home from '../containers/Home';
 
 
-  })
+import customRender from '../utils/customRender'
+
+afterEach(cleanup)
+
+it('register snapshot', () => {
+  const history = createMemoryHistory();
+  history.push('/profile');
+  const { asFragment } = customRender(<App />, { history });
+  
+  expect(asFragment(<Home />)).toMatchSnapshot()
+})
