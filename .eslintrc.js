f70e9c72b4d7da2df2c6368f296618e2ef0a6397
Ritam Verma <ritamvvv@gmail.com>
2020-11-25 12:28:10 +0100

    yml file

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     'browser': true,
     'es6': true,
     'node': true,
-    'jest': true
+    'jest': true,
+    'yaml': true
   },
   'extends': 'eslint:recommended',
   'globals': {
