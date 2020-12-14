ddee46da8f1f1aba7b889ce1d221383753629fa4
Mandy Nijssen <nijssenmandy89@gmail.com>
2020-12-14 15:02:13 +0100

    build(client): prepare client and server for deployment

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = require('./router');
 const cors = require('cors');
+const path = require('path');
 
 const morgan = require('morgan');
 const app = express();
 
 // app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(router);
---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  port: 4002,
+  db_name: 'wename',
+  db_user: 'mandynijssen',
+  db_password: 'admin',
+  db_host: 'localhost',
+  secret_key: 'very secret key'
+};
