a56e02a7b830e5ae9409c429f1d2e5204c243b52
Mandy Nijssen <nijssenmandy89@gmail.com>
2020-12-14 18:52:58 +0100

    server: change order middleware

--- server/app.js
+++ server/app.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(router);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.use(router);
 
 module.exports = app;
